3eefd064a862eb0266d63e017f605d38b6d35a3a
alisadikinsyahrizal <alisadikinsyahrizal@gmail.com>
2021-07-20 14:57:41 +0700

    fix api Ongkir

--- app/Http/Controllers/Api/RajaOngkirController.php
+++ app/Http/Controllers/Api/RajaOngkirController.php
@@ -22,7 +22,7 @@ class RajaOngkirController extends Controller
 
     public function getCities(Request $request)
     {
-        $city = City::where('fk_provinsi_id', $request->province_id)->get();
+        $city = City::where('fk_province_id', $request->province_id)->get();
         return response()->json([
             'success' => true,
             'message' => 'List data Cities By Provinces',
